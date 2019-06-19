f97ff0576b117c5d2d3e4beb9529fb1d94e735ec
hsyliark <sungyoonhwang3@gmail.com>
2019-06-19 16:47:05 +0900

    ch02

--- ch02/ch02_1_script.R
+++ ch02/ch02_1_script.R
@@ -54,15 +54,74 @@ load("data.rda")
 tableV5 <- table(data$V5)
 tableV5
 barplot(tableV5,main="출생아(남자)별 빈도",xlab="출생아수",ylab="빈도")
+df_tableV5 <- as.data.frame(tableV5)
+num <- 1:13
+df_tableV5 <- cbind.data.frame(df_tableV5,num)
+colnames(df_tableV5) <- c("born","freq","num")
+df_tableV5$born <- factor(df_tableV5$born, levels = df_tableV5$born[order(df_tableV5$num)])
+df_tableV5 <- data.frame(born=born,freq=df_tableV5$Freq)
+ggplot(df_tableV5,aes(x=born,y=freq,fill=born)) +
+  geom_bar(stat="identity") +
+  xlab("출생아수") + ylab("빈도") +
+  ggtitle("출생아(남자)별 빈도") +
+  theme(plot.title=element_text(color="darkblue",size=16,
+                                face="bold",hjust=0.5))
+  
+
 tableV1.V4 <- table(data$V1,data$V4)
 tableV1.V4
 barplot(tableV1.V4,legend.text=T,col=c("orange","green"),
         main="학력에 따른 성별 인원수",xlab="학력",ylab="빈도")
+df_tableV1.V4 <- as.data.frame(tableV1.V4)
+df_tableV1.V4$Var1 <- factor(df_tableV1.V4$Var1, levels = df_tableV1.V4$Var1[order(1:2)])
+df_tableV1.V4$Var2 <- factor(df_tableV1.V4$Var2, levels = df_tableV1.V4$Var2[order(1:8)])
+colnames(df_tableV1.V4) <- c("성별","학력","인원수")
+options(scipen=6)
+ggplot(df_tableV1.V4,aes(x=학력,y=인원수)) +
+  geom_bar(aes(fill=성별),stat="identity") +
+  xlab("학력") + ylab("인원수") +
+  ggtitle("학력에 따른 성별 인원수") +
+  theme(plot.title=element_text(color="darkblue",size=16,
+                                face="bold",hjust=0.5))
+  
 hist(data$V2,main="연령별 분포",xlab="연령",ylab="빈도")
 hist(data$V2,breaks=c(seq(0,90,10)),right=F,
      main="연령별 분포",xlab="연령",ylab="빈도")
 hist(data$V2,probability=T,
      main="연령별 분포",xlab="연령",ylab="밀도")
-
+age <- as.data.frame(data$V2)
+colnames(age) <- "age"
+ggplot(age,aes(age,fill=I("green"),col=I("red"))) +
+  geom_histogram(binwidth=5) +
+  xlab("연령") + ylab("빈도") +
+  ggtitle("연령별 분포") +
+  theme(plot.title=element_text(color="blue",size=16,
+                                face="bold",hjust=0.5))
+  
 pie(table(data$V4),main="학력수준별 비중",cex=0.8)
 table(data$V4)
+edu <- as.data.frame(table(data$V4))
+edu$Var1 <- factor(edu$Var1, levels = edu$Var1[order(1:8)])
+library(dplyr)
+edu <- edu %>%
+  arrange(desc(Var1)) %>%
+  mutate(edu.ylab=0.5*Freq) %>%
+  mutate(edu.ylab2=cumsum(Freq)-0.5*Freq) %>%
+  mutate(pct=paste(round(Freq/sum(Freq)*100,2),"%"))
+colnames(edu) <- c("학력","빈도","lab1","lab2","pct")
+ggplot(edu,aes(x="",y=빈도,fill=학력)) +
+  geom_bar(width=1,stat="identity",color="white") +
+  coord_polar("y",start=0) +
+  geom_text(aes(y=lab2,label=pct),color="black") +
+  theme_void() +
+  ggtitle("학력수준별 비중") +
+  theme(plot.title=element_text(color="navy",size=16,
+                                face="bold",hjust=0.5))
+ggplot(edu,aes(x=학력,y=빈도,fill=학력)) +
+  geom_bar(width=1,stat="identity",color="white") +
+  coord_polar() +
+  geom_text(aes(y=lab1,label=pct),color="black") +
+  theme_void() +
+  ggtitle("학력수준별 비중") +
+  theme(plot.title=element_text(color="navy",size=16,
+                                face="bold.italic",hjust=0.5))
