fa503fcd93ffd06ef0eabab22328c5385ab1956f
hsyliark <sungyoonhwang3@gmail.com>
2019-06-20 14:47:29 +0900

    ch03

--- ch03/ch03_script.R
+++ ch03/ch03_script.R
@@ -67,3 +67,23 @@ sigma <- 1
 (p0.025 <- qnorm(0.025,mean=mu,sd=sigma))
 pnorm(1.645) - pnorm(-1.645)
 pnorm(1.96) - pnorm(-1.96)
+
+
+# 그림 3-17
+
+z <- seq(-3,3,by=0.001)
+z.p <- dnorm(z)
+plot(z,z.p,axes=F,type="l",
+     main="표준정규분포(95%)",ylab="",ylim=c(-0.04,0.4))
+axis(1)
+lines(c(-3,3),c(0,0))
+points(-1.96,-0.02,pch=17,col="red")
+text(-1.96,-0.035,"-1.96",col="red")
+points(1.96,-0.02,pch=17,col="red")
+text(1.96,-0.035,"1.96",col="red")
+s <- seq(-1.96,1.96,by=0.001)
+s.z <- dnorm(s,mean=0,sd=1)
+s <- c(-1.96,s,1.96)
+s.z <- c(0,s.z,0)
+polygon(s,s.z,col="red",density=10,angle=305)
+
